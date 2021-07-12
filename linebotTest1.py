b8c44592d764defc0e3c35c0e198d5c2c20dbe6b
g1991221 <g1991221@gmail.com>
2021-07-12 21:46:40 +0800

    fix Procfile.txt

--- linebotTest1.py
+++ linebotTest1.py
@@ -13,6 +13,7 @@ from StockSearch import Func_PTTStock_TopN
 line_bot_api = LineBotApi('QcRH4+cmpgKeP24rDsHblYBgd0qkifKrgJem7GxmHyXCYLvOdZqsUkLFASyAYhjRAiFkeiY8AYd+aF2fW9Zn1FcUc9QBB4AK7AATm1MVc47orHkod3ZAm8hAOsGOLcoSy1XeyZuk+2fN8Afccu97EwdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('976067291be71b6c3e6a3d5c161db416')
 
+# 接收到LINE發過來的資訊
 @app.route("/callback",methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
